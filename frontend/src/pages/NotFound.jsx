d804c67da024c9061b1ba6e9a89bf1dffa2ec9c6
Flavia Martinez <flavia.martinez@uc.cl>
2025-03-02 13:48:54 -0300

    Not Found page

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import Button from '../components/Button'
+import styles from './NotFound.module.css'
 import { Link } from 'react-router-dom'
 
 const NotFound = () => {
   return (
-    <>
-      <div>NotFound</div>
-      <Link to='/'>Volver a inicio</Link>
-    </>
+    <div className={styles.container}>
+      <div className={styles.iconContainer}>
+        <img src='../src/assets/icons/error.png' alt='' className={styles.icon} />
+      </div>
+      <h2 className={styles.title}>404 - Oops... Página no encontrada</h2>
+      <p>Lo sentimos, la página que buscas no existe o ha sido movida.</p>
+      <Link to='/' className={styles.homeButton}>Volver al inicio</Link>
+    </div>
   )
 }
 
