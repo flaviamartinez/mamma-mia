0045c12d3a85fe26cc94e9dcb1b2219ec4b43a6b
Flavia Martinez <flavia.martinez@uc.cl>
2025-03-02 14:06:17 -0300

    Profile component

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import styles from './Profile.module.css'
 
 const Profile = () => {
   return (
-    <div>Profile</div>
+    <div className={styles.profileContainer}>
+      <h1 className={styles.title}>Tu cuenta</h1>
+      <p>Usuario: jane.doe@gmail.com</p>
+      <button className={styles.logoutButton}>Cerrar sesión</button>
+    </div>
   )
 }
 
